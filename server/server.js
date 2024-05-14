04934de291c72ab873cbb2c714b02b40fe6e5550
Enock Omweno <Eomweno@travelers.com>
2024-05-14 15:20:06 -0400

    created a new route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.get('api/planet', async(req,res) => {
-    
+app.get('/api/planet', async(req,res) => {
+    try{
+        const planets= {};
+        res.json(planets);
+    } catch(err){
+        console.err("Error;", err)
+    }
 })
