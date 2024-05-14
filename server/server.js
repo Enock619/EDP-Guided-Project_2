0532c158b0c4f5fdadd02dbd6da6791313f80bca
Dagi Girma <dagemog1993@gmail.com>
2024-05-14 16:57:47 -0400

    Added more API routes

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+// API gateway for planets.
 app.get('/api/planet', async(req,res) => {
     
     try{
@@ -21,6 +22,58 @@ app.get('/api/planet', async(req,res) => {
         const planets= await collection.find({}).toArray();
         res.json(planets);
     } catch(err){
-        console.error("Error;", err)
+        console.error("Error loading planets", err)
     }
 })
+
+
+// API gateway for characters.
+app.get('/api/characters', async(req,res) => {
+    
+    try{
+        const client= await MongoClient.connect(url);
+        const dataBase= client.db(dbName);
+        const collection = dataBase.collection(process.env.MONGO_DB_Characters_COLLECTION);
+        const characters= await collection.find({}).toArray();
+        res.json(characters);
+    } catch(err){
+        console.error("Error loading characters", err)
+    }
+})
+
+
+// API gateway for films.
+app.get('/api/films', async(req,res) => {
+    
+    try{
+        const client= await MongoClient.connect(url);
+        const dataBase= client.db(dbName);
+        const collection = dataBase.collection(process.env.MONGO_DB_Film_COLLECTION);
+        const films= await collection.find({}).toArray();
+        res.json(films);
+    } catch(err){
+        console.error("Error loading films", err)
+    }
+})
+
+app.get('/api/characters/:id', async(req,res) => {
+    
+    try{
+        const client= await MongoClient.connect(url);
+        const dataBase= client.db(dbName);
+        const collection = dataBase.collection(process.env.MONGO_DB_Characters_COLLECTION);
+        const {id} = req.params;
+        const objectId = new ObjectId({id});
+        console.log("The id requested is " + id);
+        const characters= await collection.find({id:objectId}).toArray();
+        
+        console.log("The response from mongoDb is " + characters);
+
+        res.json(characters);
+    } catch(err){
+        console.error("Error loading characters", err)
+    }
+})
+
+
+
