d1e636d756314c425934387007a1014936bf5051
Dagi Girma <dagemog1993@gmail.com>
2024-05-14 16:03:04 -0400

    added mongodb feature

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 import express from 'express'
-
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
 const app=express();
 const PORT=3000;
+
+dotenv.config();
+const url= process.env.MONGO_DB_URL;
+const dbName= process.env.MONGO_DB;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 app.get('/api/planet', async(req,res) => {
+    
     try{
-        const planets= {};
+        const client= await MongoClient.connect(url);
+        const dataBase= client.db(dbName);
+        const collection = dataBase.collection(process.env.MONGO_DB_Planets_COLLECTION );
+        const planets= await collection.find({}).toArray();
         res.json(planets);
     } catch(err){
-        console.err("Error;", err)
+        console.error("Error;", err)
     }
 })
